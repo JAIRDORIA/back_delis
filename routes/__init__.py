17332deb7dc944698c50efe697417c2d58689dfe
froilangutierrez00 <froilanguitierrez00@gmail.com>
2026-03-26 16:05:14 -0500

    Guardar init de rutas antes del merge

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,12 @@
 # __init__.py
+# __init__.py
 from .ventas import ventas_bp
 from .cortes import cortes_bp
 from .usuarios import usuarios_bp
 from .productos import productos_bp
 from .inventario import inventario_bp
-
+from .combos import combos_bp
+from .clientes import clientes_bp
 
 
 def cargarRuta(app):
@@ -13,6 +15,10 @@ def cargarRuta(app):
     app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
     app.register_blueprint(productos_bp, url_prefix='/productos')
     app.register_blueprint(inventario_bp, url_prefix='/inventarios')
+    app.register_blueprint(combos_bp, url_prefix='/combos')
+    app.register_blueprint(clientes_bp, url_prefix='/clientes')
+    
+
 
     
     
