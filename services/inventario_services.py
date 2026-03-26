fd1a9c05e4b0286b78588d91f6844e90c0e59280
Dar <cantillolunad@gmail.com>
2026-03-26 15:15:17 -0500

    Se agrega módulo de inventario completo

---
+++ services/inventario_services.py
@@ -0,0 +1,22 @@
+from flask import current_app
+from models.inventario_model import inventarios
+
+def listado_inventarios():
+    c = current_app.mysql.connection.cursor()
+    sql = "SELECT id, producto_id, stock_actual, unidades_sueltas, stock_minimo, updated_at FROM inventario"
+    c.execute(sql)
+    datos = c.fetchall()
+    
+    lista = []
+    for p in datos:
+        inventario = inventarios(
+            id               = p[0],
+            producto_id      = p[1],
+            stock_actual     = p[2],
+            unidades_sueltas = p[3],
+            stock_minimo     = p[4],
+            updated_at       = p[5]
+        ).toDic()
+        lista.append(inventario)
+    
+    return lista               
