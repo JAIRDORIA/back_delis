dcbc0e17ebf648087faf4795bac5c7996cfdd790
Jair Doria <jairdoria2020@gmail.com>
2026-04-02 19:43:14 -0500

    registro de abono actualizado correctamente

--- models/abonos_model.py
+++ models/abonos_model.py
@@ -1,6 +1,6 @@
 class abonos:
-    def __init__(self,id,venta_id,corte_id,usuario_id,monto,fecha,observacion):
-        self.ID_abonos = id
+    def __init__(self,venta_id,corte_id,usuario_id,monto,fecha,observacion):
+        
         self.venta_id = venta_id
         self.corte_id = corte_id
         self.usuario_id = usuario_id
@@ -11,7 +11,7 @@ class abonos:
     
     def todic(self):
         return{
-            "id" : self.ID_abonos,
+            
             "id_venta" : self.venta_id ,
             "id_corte" : self.corte_id ,
             "id_usuario" : self.usuario_id ,
