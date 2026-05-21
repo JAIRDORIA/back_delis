dfd5889e6a42f558c1aa282f3c54c8f20cc84245
Dar <cantillolunad@gmail.com>
2026-05-20 20:46:03 -0500

    cambios de usuarios

--- routes/ventas.py
+++ routes/ventas.py
@@ -1,27 +1,28 @@
 from flask import Blueprint
 from controllers.venta_controller import cntListado,cntregistrar, cntActualizar, cntAnular
 from flask_jwt_extended import jwt_required
-
+from utils.decorators import token_requerido, rol_requerido
 ventas_bp = Blueprint ('ventas', __name__)
 
 
 @ventas_bp.route('/')
+@token_requerido
 def listado():
     return cntListado()
 
 @ventas_bp.route('/', methods = ["POST"])
-#@jwt_required()
+@token_requerido
 def registro():
     return cntregistrar()
 
 
 @ventas_bp.route('/<int:id>', methods=['PUT'])
-#@jwt_required()
+@token_requerido
 def actualizar(id):
     return cntActualizar(id)
 
 
 @ventas_bp.route('/<int:id>/anulacion', methods=['PUT'])
-#@jwt_required()
+@token_requerido
 def anular(id):
     return cntAnular(id)
