0c3f510502e6cf74cf291ce7bcea8e7e7db6b995
Jair Doria <jairdoria2020@gmail.com>
2026-06-19 07:52:58 -0500

    agregando token

--- routes/auditoria.py
+++ routes/auditoria.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from controllers.auditoria_controller import cntListado
+from utils.decorators import token_requerido
 
 auditoria_bp = Blueprint('auditoria', __name__)
 
 @auditoria_bp.route('/', methods=['GET'])
+@token_requerido
 def listado():
     return cntListado()
