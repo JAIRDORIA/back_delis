f2619fceb9c61d3adc8dbaddac7c7d482fe4bb42
Jair Doria <jairdoria2020@gmail.com>
2026-06-24 09:30:51 -0500

    agregando cambios para id en cliente1

--- controllers/clientes_controller.py
+++ controllers/clientes_controller.py
@@ -92,7 +92,7 @@ def cntRegistrar():
             return jsonify({"mensaje": "Este correo ya se encuentra registrado"}), 400
 
     # 5. Llamar al servicio
-    resultado = crear_clientes(nombre, telefono, direccion, email)
+    resultado = crear_clientes(nombre, telefono, direccion, email,identificacion)
     
     if isinstance(resultado, tuple):
         return jsonify(resultado[0]), resultado[1]
