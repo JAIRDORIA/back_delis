459bd2970a5a4f154daea0d2f1259e4397a4f3a4
Jair Doria <jairdoria2020@gmail.com>
2026-07-18 13:00:38 -0500

    creando get de clientes

--- controllers/abonos_controller.py
+++ controllers/abonos_controller.py
@@ -16,7 +16,7 @@ def cntListado():
         if pagina < 1:
             return jsonify({"mensaje": "la pagina debe ser mayor a 0"}), 400
         if limite < 1 or limite > 200:
-            return jsonify({"mensaje": "el limite debe ser entre 1 y 100"}), 400
+            return jsonify({"mensaje": "el limite debe ser entre 1 y 101"}), 400
 
         datos = listado_abonos(pagina, limite)
         return jsonify(datos), 200
